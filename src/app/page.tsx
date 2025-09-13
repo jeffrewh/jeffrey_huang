5a52ed34ded5486fc96b63613f6e6fec1da5c390
Jeffrey <68975522+jeffrewh@users.noreply.github.com>
2025-09-13 03:13:25 -0700

    added project mobile sticky header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,11 @@ export default function Home() {
               <p className="mb-4 text-base text-slate-400 leading-relaxed">
                 Here I detail my ceaseless journey in becoming the
                 comprehensive, multi-disciplined engineer. I aim to become
-                self-sufficient and a jack of all trades—don't finish that
-                phrase. I'm an aspiring engineer, passionate in nigh all things
-                learning, and thusly, tend to get my fingers stuck in too many
-                pies. I liken it to &quot;knowledge-FOMO&quot;, if you will.
+                self-sufficient and a jack of all trades—don&apos;t finish that
+                phrase. I&apos;m an aspiring engineer, passionate in nigh all
+                things learning, and thusly, tend to get my fingers stuck in too
+                many pies. I liken it to &quot;knowledge-FOMO&quot;, if you
+                will.
               </p>
               <p className="mb-4 text-base text-slate-400 leading-relaxed">
                 Currently, I am a Systems Engineer at{" "}
@@ -269,6 +270,11 @@ export default function Home() {
             className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
           >
             {/* ... Sticky Header for Projects ... */}
+            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+                Projects
+              </h2>
+            </div>
             <div>
               <ul className="group/list">
                 {/* === RASPBERRY PI PROJECT CARD === */}
